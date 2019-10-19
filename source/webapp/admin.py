cdfb712d9543da9115708313762220dd4e178bce
Evgenii <lipatowevgeniy@gmail.com>
2019-10-19 12:12:39 +0600

    Models were created. Fixture were done

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'question', 'created_at']
+    list_filter = ['question']
+    list_display_links = ['pk']
+    search_fields = ['question']
+    readonly_fields = ['created_at']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'varText', 'poll']
+    list_filter = ['varText']
+    list_display_links = ['pk']
+    search_fields = ['varText']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+
+
 # Register your models here.
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=500, null=False, blank=False, verbose_name='question')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creation Time')
+
+
+class Choice(models.Model):
+    varText = models.TextField(max_length=1000, null=False, blank=False, verbose_name='answer')
+    poll = models.ForeignKey('webapp.Poll', related_name='polls', on_delete=models.CASCADE)
